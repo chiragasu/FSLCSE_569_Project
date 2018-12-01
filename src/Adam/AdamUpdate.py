5168f9ec5e95d45aafe5cdfbb3aca5d47ba0a1ba
chiragasu <45501869+chiragasu@users.noreply.github.com>
2018-11-30 17:51:14 -0700

    AdamUpdate.py
    Changed for adam update parameters

--- src/Adam/AdamUpdate.py
+++ src/Adam/AdamUpdate.py
@@ -1,7 +1,10 @@
 
 
 beta = 0.9;
+beta2 = 0.999;
 def adamUpdateParams(mW, mb, dW, db):
     mW = beta * mW + (1 - beta) * dW;
     mb = beta * mb + (1 - beta) * db;
+    vW = beta2 * vW + (1 - beta2) * dW * dW;
+    vb = beta2 * vb + (1 - beta2) * db * db;
     return mW, mb;

cd6a6dbc6229c7023ab2a24a4f872772aac4aea5
chiragasu <45501869+chiragasu@users.noreply.github.com>
2018-11-30 17:37:23 -0700

    Update RMSProp.py
    Changed the update parameter function

--- src/RMSProp/RMSpropUpdate.py
+++ src/RMSProp/RMSpropUpdate.py
@@ -2,6 +2,6 @@
 
 beta = 0.9;
 def rmsPropUpdateParams(mW, mb, dW, db):
-    mW = beta * mW + (1 - beta) * dW;
-    mb = beta * mb + (1 - beta) * db;
+    mW = beta * mW + (1 - beta) * dW * dW;
+    mb = beta * mb + (1 - beta) * db * db;
     return mW, mb;
